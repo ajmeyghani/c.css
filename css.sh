e41f557320e78322ff1a11ee723c50a26e2ba6ab
AJ Meyghani <meyghania@gmail.com>
2020-04-05 00:55:45 -0700

    remove rollup, use shell script

---
+++ css.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf dist
+mkdir -p dist
+cat node_modules/minireset.css/minireset.css >> dist/modern.css
+cat src/{variables,type,layout,tables,lists}.css >> dist/modern.css
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./variables.css";
-// import "./type.css";
-// import "./layout.css";
-// import "./tables.css";
-// import "./lists.css";
-// import "./page.css";
