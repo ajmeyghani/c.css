27425699af6035de8d160a179f6cd8da09c910da
AJ Meyghani <meyghania@gmail.com>
2020-04-06 23:46:22 -0700

    add some docs for helper layout classes.

--- css.sh
+++ css.sh
@@ -2,4 +2,4 @@
 rm -rf dist
 mkdir -p dist
 cat node_modules/minireset.css/minireset.css >> dist/modern.css
-cat src/{variables,type,layout,tables,lists}.css >> dist/modern.css
+cat src/{variables,type,misc,layout,tables,lists}.css >> dist/modern.css
