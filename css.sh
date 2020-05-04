10ebcefea3b1ffdb4cbc80c776859472c36e3b83
AJ Meyghani <meyghania@gmail.com>
2020-05-03 21:28:33 -0700

    remove old config for bundling css

--- css.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf dist
-mkdir -p dist
-cat node_modules/minireset.css/minireset.css >> dist/modern.css
-cat src/{variables,type,misc,layout,tables,lists}.css >> dist/modern.css
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
       ignoreOrder: true,
     }),
   ],
+
   module: {
     rules: [
       {
